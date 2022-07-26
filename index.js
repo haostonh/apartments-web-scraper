9d7c2513c66f77ab59c08d58c34196004ce33ef3
haostonh <hsthuynh@ucdavis.edu>
2022-07-25 17:59:59 -0700

    add axios to get info from website

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const PORT = "8080";
+const PORT = "8080"
 
-const axios = require("axios");
-const cheerio = require("cheerio");
-const express = require("express");
+const axios = require("axios")
+const cheerio = require("cheerio")
+const express = require("express")
 
-const webScraper = express();
+const webScraper = express()
 
-const url = "https://www.apartments.com/davis-ca/";
+const url = "https://www.apartments.com/davis-ca/"
 
 // Sending a GET request for the info on website
 axios(url)
+  .then(response => {
+    const html = response.data
+    console.log(html)
+  })
 
 webScraper.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
