91b0eb404811a94f3fcf06fdf225c72d1bb38642
haostonh <hsthuynh@ucdavis.edu>
2022-07-25 22:17:38 -0700

    change function name to be specific

--- index.js
+++ index.js
@@ -6,38 +6,42 @@ const express = require("express")
 
 const webScraper = express()
 
-const url = "https://www.apartments.com/dasdadasd/"
+const url = "https://www.apartments.com/davis-ca/"
 
-// Sending a GET request for the info on website
-axios(url)
-  .then(response => {
-    const html = response.data
-    const $ = cheerio.load(html)
-    const apartments = []
+function getApartmentInfo() {
+  // Sending a GET request for the info on website
+  axios(url)
+    .then(response => {
+      const html = response.data
+      const $ = cheerio.load(html)
+      const apartments = []
 
-    $('.placard', html).each(function() {
-      // needs a dash looking at page inspect
-      // there were property-title classes in other sections, causing blank strings
-      const aptName = $('.property-title', this).text()
-      const aptAddress = $('.property-address', this).text()
-      const aptPhoneNumber = $('.phone-link', this).find('span').text()
-      const aptPricing = $('.property-pricing', this).text()
-      const aptBeds = $('.property-beds', this).text()
-      const aptLink = $('.property-information', this).find('a').attr('href')
+      $('.placard', html).each(function() {
+        // needs a dash looking at page inspect
+        // there were property-title classes in other sections, causing blank strings
+        const aptName = $('.property-title', this).text()
+        const aptAddress = $('.property-address', this).text()
+        const aptPhoneNumber = $('.phone-link', this).find('span').text()
+        const aptPricing = $('.property-pricing', this).text()
+        const aptBeds = $('.property-beds', this).text()
+        const aptLink = $('.property-information', this).find('a').attr('href')
 
-      apartments.push({
-        aptName,
-        aptAddress,
-        aptPhoneNumber,
-        aptPricing,
-        aptBeds,
-        aptLink
+        apartments.push({
+          aptName,
+          aptAddress,
+          aptPhoneNumber,
+          aptPricing,
+          aptBeds,
+          aptLink
+        })
       })
-    })
 
-    console.log(apartments)
-  }).catch(err => 
-    console.log("Error Status:", err.response.status)
-    )
+      console.log(apartments)
+    }).catch(err => 
+      console.log("Error Status:", err.response.status)
+      )
+}
 
 webScraper.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
+
+getApartmentInfo();
